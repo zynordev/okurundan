cc9ee3abb68c12ef0d6dc47b67573f0e8d00f8a1
zynordev <159820410+zynordev@users.noreply.github.com>
2026-01-04 14:09:54 +0300

    Delete script.sh

--- script.sh
+++
@@ -1,7 +0,0 @@
-# 1. YANLIŞ PAKET İSMİNİ DÜZELTME ve GEREKLİ TÜM KÜTÜPHANELERİ YÜKLEME
-npm install express-rate-limit helmet morgan jsonwebtoken bcryptjs --unsafe-perm
-
-# Veya, klasör izinlerini düzeltin:
-sudo chown -R $(whoami) /home/admin/okurundanv2.1/
-npm install express-rate-limit helmet morgan jsonwebtoken bcryptjs
-
